447209cdd123107f228f421ec00acd82f00267a9
Venky7032 <venky703236@gmail.com>
2022-06-23 10:20:35 +0530

    Comments

--- Cucumber_DataTable/src/test/java/runner/testrunner.java
+++ Cucumber_DataTable/src/test/java/runner/testrunner.java
@@ -5,6 +5,14 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+
+
+
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
 features = {"classpath:features/Nationality.feature"},
